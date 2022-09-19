d655a3b058ab19b486fe784152d3db538d871444
Catherine Yu <catyu6000@gmail.com>
2022-09-19 16:51:41 -0700

    update notion post id

--- src/Post.js
+++ src/Post.js
@@ -10,6 +10,6 @@ function Post(path, title, date, image, blurb, notionId) {
 }
 
 const firstPost = new Post("first-post", "Starting a personal website...", "12/28/2020", 
-  placeholder, "Welcome to my personal website!", "15a308fb9bf64345b05e1ec1a7dce6ae");
+  placeholder, "Welcome to my personal website!", "fd0fa58588e04c1cbb30a45917bc6712");
 
 export const posts = [firstPost]
